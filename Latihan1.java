7f1b180c4993156955f9fc5ffa2d7d9a01c2d2b7
ReizaHD <126785335+ReizaHD@users.noreply.github.com>
2023-03-02 19:59:43 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Latihan1 {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Halo, nama saya adalah Reiza Hersa Dwitama");
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.print("Saya sedang menulis program Java");
+	}
+
+}
