73e1829416553f3e4acce5f9c1f88445a906a5b1
ReizaHD <126785335+ReizaHD@users.noreply.github.com>
2023-04-08 20:26:46 +0700

    Create Sepeda.java

---
+++ praktikum6/transportasi/Sepeda.java
@@ -0,0 +1,14 @@
+package transportasi;
+public class Sepeda{
+	public Sepeda(String brand, int gear){
+		System.out.println("----Sepeda----");
+		System.out.println("Brand = "+brand);
+		System.out.println("Jumlah Gear = "+gear);
+		System.out.println("--------------");
+	}
+	
+	public void bel(){
+		System.out.println("Sepeda : Bel dibunyikan");
+	}
+}
+		
