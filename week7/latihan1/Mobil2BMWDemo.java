be73f07dbe9cbce3077fec346be51f05b36a27d4
ReizaHD <126785335+ReizaHD@users.noreply.github.com>
2023-04-18 21:42:34 +0700

    Add files via upload

---
+++ week7/latihan1/Mobil2BMWDemo.java
@@ -0,0 +1,11 @@
+package latihan1;
+
+public class Mobil2BMWDemo {
+    public static void main(String[] args){
+        Mobil2BMW bmw = new Mobil2BMW();
+        bmw.hidupkanMobil();
+        bmw.nontonTV();
+        bmw.ubahGigi();
+        bmw.matikanMobil();
+    }
+}
