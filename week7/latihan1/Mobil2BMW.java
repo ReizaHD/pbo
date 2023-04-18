998aadfed5456ef44e8c62fe1c1325c81bd96862
ReizaHD <126785335+ReizaHD@users.noreply.github.com>
2023-04-18 21:47:15 +0700

    Delete week7/latihan1 directory

--- week7/latihan1/Mobil2BMW.java
+++
@@ -1,9 +0,0 @@
-package latihan1;
-
-public class Mobil2BMW extends Mobil2{
-    public void nontonTV(){
-        System.out.println("TV Dihidupkan...");
-        System.out.println("TV mencari channel...");
-        System.out.println("TV menampilkan gambar...");
-    }
-}
